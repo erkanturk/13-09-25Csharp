22be90f34365920e6a47f7e5d2f0a7ab9e6d57b3
erkanturk <Refres98@gmail.com>
2025-11-29 15:46:11 +0300

    Ado.net

---
+++ Csharp/46_Ado.Net/Data/Execute.cs
@@ -0,0 +1,87 @@
+﻿using _46_Ado.Net.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_Ado.Net.Data
+{
+    public class Execute
+    {
+        private readonly SqlConnection con;
+        SqlCommand cmd;
+        public Execute()
+        {
+            con=new SqlConnection//Dependency injection Dışa bağımlılık
+                (@"Server=202-HOCAPC\SQLDERS;Database=OkulDb;uid=sa;pwd=1;TrustServerCertificate=true");
+        }
+        public int OgretmenEkle()
+        {
+            var eks = 0;
+            try
+            {
+                SqlCommand cmd = new SqlCommand($"Insert into Ogretmenler (Ad,Soyad,Yas,Brans) Values ('Erkan','Türk','31','Yazılım Mühendisi')", con);
+                con.Open();
+                eks=cmd.ExecuteNonQuery();//Crud r haricinde işlemleri gerçekleştirir
+                //Create Update Delete 
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Hata: "+ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return eks;
+        }
+        public void OgretmenEkle(Ogretmen ogretmen)
+        {
+
+            SqlCommand cmd = new SqlCommand($"Insert into Ogretmenler (Ad,Soyad,Yas,Brans) Values ('{ogretmen.Ad}','{ogretmen.Soyad}','{ogretmen.Yas}','{ogretmen.Brans}')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public SqlDataReader Listele()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Ogretmenler", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();//Read işlemi crud daki
+                return reader;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+               
+            }
+
+           
+            
+        }
+        public SqlDataReader Listele(string ad)
+        {
+            if (ad==null)
+            {
+                Console.WriteLine("Aradığınız kullanıcı bilgisine ulaşılamadı");
+            }
+            else
+            {
+                cmd= new SqlCommand($"Select * from Ogretmenler where Ad like '%{ad}%' ", con);
+            }
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+    }
+}
---
+++ Csharp/46_Ado.Net/Program.cs
@@ -0,0 +1,48 @@
+﻿using _46_Ado.Net.Data;
+using _46_Ado.Net.Model;
+using System.Data.SqlClient;
+
+namespace _46_Ado.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogretmen ogretmen = new Ogretmen();
+            Execute execute = new Execute();
+            //execute.OgretmenEkle();
+            //Console.WriteLine("Ad");
+            //ogretmen.Ad=Console.ReadLine();
+            //Console.WriteLine("Soyad");
+            //ogretmen.Soyad=Console.ReadLine();
+            //Console.WriteLine("Yaş");
+            //ogretmen.Yas=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Branş");
+            //ogretmen.Brans=Console.ReadLine();
+            //execute.OgretmenEkle(ogretmen);
+
+
+            Console.WriteLine("Ad");
+            string ad = Console.ReadLine();
+            //SqlDataReader reader = execute.Listele();
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine(reader[0].ToString());
+            //    Console.WriteLine(reader[1].ToString());
+            //    Console.WriteLine(reader[2].ToString());
+            //    Console.WriteLine(reader[3].ToString());
+            //    Console.WriteLine(reader[4].ToString());
+            //}
+
+            SqlDataReader reader2 = execute.Listele(ad);
+            while (reader2.Read())
+            {
+                Console.WriteLine(reader2[0].ToString());
+                Console.WriteLine(reader2[1].ToString());
+                Console.WriteLine(reader2[2].ToString());
+                Console.WriteLine(reader2[3].ToString());
+                Console.WriteLine(reader2[4].ToString());
+            }
+        }
+    }
+}
